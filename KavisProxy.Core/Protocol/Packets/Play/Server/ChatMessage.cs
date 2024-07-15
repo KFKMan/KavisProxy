546fb05b717ee0b6a505faa22084509fdeca2f47
Kaan Faruk Kınalı <62002518+KFKMan@users.noreply.github.com>
2024-07-15 12:36:16 +0300

    TimeUpdate and ChatMessage Packet

---
+++ KavisProxy.Core/Protocol/Packets/Play/Server/ChatMessage.cs
@@ -0,0 +1,36 @@
+﻿namespace KavisProxy.Core.Protocol.Packets;
+
+public class ChatMessage : Packet<ChatMessageData>
+{
+    public int PacketID = 0x02;
+
+    int Packet.PacketID => throw new NotImplementedException();
+
+    public ChatMessageData Read(IByteBuffer buffer)
+    {
+        return new ChatMessageData(buffer.ReadString(), buffer.ReadByte());
+    }
+
+    public void Write(IByteBuffer buffer, ChatMessageData value)
+    {
+        buffer.WriteString(value.JSONData);
+        buffer.WriteByte(value.Position);
+    }
+}
+
+public record ChatMessageData(string JSONData, byte Position)
+{
+
+    public enum PositionType : int
+    {
+        /// <summary>
+        /// ChatBox
+        /// </summary>
+        Chat = 0,
+        /// <summary>
+        /// ChatBox
+        /// </summary>
+        SystemMessage = 1,
+        AboveHotbar = 2
+    }
+}
---
+++ KavisProxy.Core/Protocol/Packets/Play/Server/TimeUpdate.cs
@@ -0,0 +1,24 @@
+﻿namespace KavisProxy.Core.Protocol.Packets;
+
+public class TimeUpdate : Packet<TimeUpdateData>
+{
+    public int PacketID => 0x03;
+
+    public TimeUpdateData Read(IByteBuffer buffer)
+    {
+        return new TimeUpdateData(buffer.ReadLong(), buffer.ReadLong());
+    }
+
+    public void Write(IByteBuffer buffer, TimeUpdateData value)
+    {
+        buffer.WriteLong(value.WorldAge);
+        buffer.WriteLong(value.TimeOfDay);
+    }
+}
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="WorldAge">it's Long not VarLong | In ticks; not changed by server commands</param>
+/// <param name="TimeOfDay">it's Long not VarLong | The world (or region) time, in ticks. If negative the sun will stop moving at the Math.abs of the time ??</param>
+public record TimeUpdateData(long WorldAge, long TimeOfDay);
